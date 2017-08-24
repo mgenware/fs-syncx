7a9a2f00811f20aa591b46074bfa89c43379086c
Mgen <m@mgenware.com>
2017-08-24 23:36:54 +0800

    Remove all ignoreExp arguments

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-'use strict';
 const assert = require('assert');
 const nodepath = require('path');
 const fss = require('../lib/main');
@@ -7,14 +6,12 @@ const PATH_NOT_EXIST = 'PATH_NOT_EXIST.__ABC__';
 const FILE_A_REL = 'test/data/text.txt';
 const FILE_A_ABS = nodepath.join(__dirname, 'data/text.txt');
 const FILE_A_NAME = 'text.txt';
+const FILE_B_REL = 'test/data/json.json';
 const DIR_A_REL = 'test/data/';
 
 describe('fss.pathInfo', () => {
   const t = fss.pathInfo;
 
-  it('Ignore exception', () => {
-    assert.doesNotThrow(() => t(PATH_NOT_EXIST, true));
-  });
   it('Exception', () => {
     assert.throws(() => t(PATH_NOT_EXIST, false));
   });
@@ -23,40 +20,65 @@ describe('fss.pathInfo', () => {
   });
 
   it('name [Relative path]', () => {
-    assert(t(FILE_A_REL).name === FILE_A_NAME);
+    assert.equal(t(FILE_A_REL).name, FILE_A_NAME);
   });
   it('name [Absolute path]', () => {
-    assert(t(FILE_A_ABS).name === FILE_A_NAME);
+    assert.equal(t(FILE_A_ABS).name, FILE_A_NAME);
   });
 
   it('fullPath [Relative path]', () => {
-    assert(t(FILE_A_REL).fullPath === FILE_A_ABS);
+    assert.equal(t(FILE_A_REL).fullPath, FILE_A_ABS);
   });
   it('fullPath [Absolute path]', () => {
-    assert(t(FILE_A_ABS).fullPath === FILE_A_ABS);
+    assert.equal(t(FILE_A_ABS).fullPath, FILE_A_ABS);
   });
 
   it('isFile', () => {
-    assert(t(FILE_A_REL).isFile);
+    assert.equal(t(FILE_A_REL).isFile, true);
   });
   it('isDirectory', () => {
-    assert(t(DIR_A_REL).isDirectory);
+    assert.equal(t(DIR_A_REL).isDirectory, true);
   });
 });
 
 
 describe('fss.isDirectory', () => {
   const t = fss.isDirectory;
-  it('Ignore exception', () => {
-    assert.doesNotThrow(() => t(PATH_NOT_EXIST, true));
-  });
   it('Exception', () => {
     assert.throws(() => t(PATH_NOT_EXIST, false));
   });
   it('True', () => {
-    assert(t(DIR_A_REL) === true);
+    assert.equal(t(DIR_A_REL), true);
   });
   it('False', () => {
-    assert(t(FILE_A_ABS) === false);
+    assert.equal(t(FILE_A_ABS), false);
   });
 });
+
+describe('fss.isFile', () => {
+  const t = fss.isFile;
+  it('Exception', () => {
+    assert.throws(() => t(PATH_NOT_EXIST, false));
+  });
+  it('True', () => {
+    assert.equal(t(FILE_A_REL), true);
+  });
+  it('False', () => {
+    assert.equal(t(DIR_A_REL), false);
+  });
+});
+
+describe('fss.readTextFile', () => {
+  const t = fss.readTextFile;
+  it('Default', () => {
+    assert.equal(t(FILE_A_REL), 'sample text\n');
+  });
+});
+
+describe('fss.readJSONFile', () => {
+  const t = fss.readJSONFile;
+  it('Default', () => {
+    assert.deepEqual(t(FILE_B_REL), { a: 123, b: 'test' });
+  });
+});
+
