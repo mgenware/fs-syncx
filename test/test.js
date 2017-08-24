2e4927365da20dc2c283fa626d37ea976a185865
Mgen <m@mgenware.com>
2017-08-24 23:06:29 +0800

    Fix ESLint warnings

--- test/test.js
+++ test/test.js
@@ -9,7 +9,6 @@ const FILE_A_ABS = nodepath.join(__dirname, 'data/text.txt');
 const FILE_A_NAME = 'text.txt';
 const DIR_A_REL = 'test/data/';
 
-
 describe('fss.pathInfo', () => {
   const t = fss.pathInfo;
 
@@ -24,17 +23,17 @@ describe('fss.pathInfo', () => {
   });
 
   it('name [Relative path]', () => {
-    assert(t(FILE_A_REL).name == FILE_A_NAME);
+    assert(t(FILE_A_REL).name === FILE_A_NAME);
   });
   it('name [Absolute path]', () => {
-    assert(t(FILE_A_ABS).name == FILE_A_NAME);
+    assert(t(FILE_A_ABS).name === FILE_A_NAME);
   });
 
   it('fullPath [Relative path]', () => {
-    assert(t(FILE_A_REL).fullPath == FILE_A_ABS);
+    assert(t(FILE_A_REL).fullPath === FILE_A_ABS);
   });
   it('fullPath [Absolute path]', () => {
-    assert(t(FILE_A_ABS).fullPath == FILE_A_ABS);
+    assert(t(FILE_A_ABS).fullPath === FILE_A_ABS);
   });
 
   it('isFile', () => {
@@ -42,5 +41,22 @@ describe('fss.pathInfo', () => {
   });
   it('isDirectory', () => {
     assert(t(DIR_A_REL).isDirectory);
+  });
+});
+
+
+describe('fss.isDirectory', () => {
+  const t = fss.isDirectory;
+  it('Ignore exception', () => {
+    assert.doesNotThrow(() => t(PATH_NOT_EXIST, true));
+  });
+  it('Exception', () => {
+    assert.throws(() => t(PATH_NOT_EXIST, false));
+  });
+  it('True', () => {
+    assert(t(DIR_A_REL) === true);
+  });
+  it('False', () => {
+    assert(t(FILE_A_ABS) === false);
   });
 });
