b755b747c080124a6f19240a67283635bcf1ef1d
Mgen <m@mgenware.com>
2017-08-24 20:59:44 +0800

    Test cases for pathInfo.fullPath

--- test/test.js
+++ test/test.js
@@ -7,6 +7,7 @@ const PATH_NOT_EXIST = 'PATH_NOT_EXIST.__ABC__';
 const FILE_A_REL = 'test/data/text.txt';
 const FILE_A_ABS = nodepath.join(__dirname, 'data/text.txt');
 const FILE_A_NAME = 'text.txt';
+const DIR_A_REL = 'test/data/';
 
 
 describe('fss.pathInfo', () => {
@@ -27,5 +28,19 @@ describe('fss.pathInfo', () => {
   });
   it('name [Absolute path]', () => {
     assert(t(FILE_A_ABS).name == FILE_A_NAME);
+  });
+
+  it('fullPath [Relative path]', () => {
+    assert(t(FILE_A_REL).fullPath == FILE_A_ABS);
+  });
+  it('fullPath [Absolute path]', () => {
+    assert(t(FILE_A_ABS).fullPath == FILE_A_ABS);
+  });
+
+  it('isFile', () => {
+    assert(t(FILE_A_REL).isFile);
+  });
+  it('isDirectory', () => {
+    assert(t(DIR_A_REL).isDirectory);
   });
 });
