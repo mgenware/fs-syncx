bccce70cc19224773318f2c44b9414de540d0ca3
Mgen <m@mgenware.com>
2017-10-14 00:59:51 +0800

    Add tests for checking main type definition file

--- test/test_module.ts
+++ test/test_module.ts
@@ -1,8 +1,14 @@
-const fss = require('../..');
+const main = require('../..');
 import * as assert from 'assert';
+import * as fs from 'fs';
+import * as path from 'path';
 
 describe('require this module', () => {
-  it('No exception thrown', () => {
-    assert.equal(typeof fss.listDirs, 'function');
+  it('Check a function', () => {
+    assert.equal(typeof main.listDirs, 'function');
+  });
+
+  it('Check type definition file', () => {
+    assert.equal(fs.statSync('./dist/lib/main.d.ts').isFile(), true);
   });
 });
